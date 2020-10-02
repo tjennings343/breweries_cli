b944966696725279b7f88cd8d808706b29e40f0f
tjennings343 <tjennings343@gmail.com>
2020-10-01 23:44:46 -0400

    10/1 update 2

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,7 +5,6 @@ class Cli
     def start
         puts ""
         puts "Welcome to Brewery Tour"
-        puts ""
         puts "Please enter your city to begin"
         puts ""
         @city = gets.chomp.downcase
@@ -16,23 +15,40 @@ class Cli
         puts ""
         puts "Select number to see details, or type 'exit' to start over"
         puts ""
-        user_inp = gets.chomp.downcase 
-        brewery = breweries[user_inp.to_i - 1]
-        # print_brewery_info(brewery)
-        # binding.pry
-
+        user_inp = gets.chomp.downcase
+        if user_inp.to_i > breweries.length
+            puts ""
+            print_breweries(breweries)
+            puts ""
+            puts "Sorry please try again"
+            puts ""
+            puts "Select number to see details, or type 'exit' to start over"
+            user_inp = gets.chomp.downcase
+        end
+        if user_inp != 'exit' 
+            brewery = breweries[user_inp.to_i - 1]
+            print_brewery_info(brewery)
+        elsif user_inp == 'exit'
+            puts "Thank you, come back soon"          
+        end
     end
 
     def print_breweries(br)
         puts "Local breweries in #{@city.capitalize}"
-        puts ""
+        puts ""       
         br.each_with_index do |b,i|
             puts " #{i + 1}: #{b.name}"
         end
     end
 
-    # def print_brewery_info(brew)
+     def print_brewery_info(brew)
+        puts brew.name
+        puts ""
+        puts brew.city.capitalize
+        puts brew.state
+        puts brew.street
+        puts brew.website
 
-    # end
+     end
 
 end
